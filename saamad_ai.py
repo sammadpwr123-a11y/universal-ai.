f7775c9f9181031ff49dd7c79c4106828a836419
sammadpwr123-a11y <sammadpwr123@gmail.com>
2026-04-25 05:47:42 -0700

    Update saamad_ai.py

--- saamad_ai.py
+++ saamad_ai.py
@@ -5,7 +5,7 @@ import os
 # --- 1. AI CONFIGURATION ---
 # Put your Google Gemini API Key here
 genai.configure(api_key="google API key")
-model = genai.GenerativeModel('gemini-1.5-flash-latest')
+model = genai.GenerativeModel('gemini-pro')
 
 # --- 2. APP PAGE SETTINGS ---
 st.set_page_config(page_title="Sammad AI Server", page_icon="💻")
