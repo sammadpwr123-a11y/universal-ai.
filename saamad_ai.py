89d43b0c50f89f91d4f8d693c234f34fed896200
sammadpwr123-a11y <sammadpwr123@gmail.com>
2026-04-25 05:24:12 -0700

    Update saamad_ai.py

--- saamad_ai.py
+++ saamad_ai.py
@@ -4,7 +4,7 @@ import os
 
 # --- 1. AI CONFIGURATION ---
 # Put your Google Gemini API Key here
-genai.configure(api_key="AIzaSyByQyJVWfrVuw_NVs7XfMGyn8-iWcABQzY")
+genai.configure(api_key="google API key")
 model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 # --- 2. APP PAGE SETTINGS ---
